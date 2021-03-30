7cd5f6370c8f69d7b7c76fd135e553e01f4e4bd0
xavigu <xavigu91@gmail.com>
2021-03-30 17:22:42 +0200

    Add add.page component and navigate to it from tab1 page button

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { List } from 'src/app/models/list.model';
 import { WishesService } from 'src/app/services/wishes.service';
 
@@ -11,8 +12,12 @@ export class Tab1Page {
 
   private lists: List[] = [];
 
-  constructor(public wishesService: WishesService) {
+  constructor(public wishesService: WishesService, private router: Router) {
     this.lists = wishesService.lists;
+  }
+
+  addList(){
+    this.router.navigateByUrl('/tabs/tab1/add');
   }
 
 }
