edb82e1937e4f821bc534fec9a69b7231a48ea41
xavigu <xavigu91@gmail.com>
2021-03-30 18:00:43 +0200

    Add logic to AddList button to push a new list into the lists object

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 import { List } from 'src/app/models/list.model';
 import { WishesService } from 'src/app/services/wishes.service';
 
@@ -10,14 +11,42 @@ import { WishesService } from 'src/app/services/wishes.service';
 })
 export class Tab1Page {
 
-  private lists: List[] = [];
 
-  constructor(public wishesService: WishesService, private router: Router) {
-    this.lists = wishesService.lists;
+  constructor(public wishesService: WishesService, private router: Router, private alertCtrl: AlertController) {
   }
 
-  addList(){
-    this.router.navigateByUrl('/tabs/tab1/add');
+  async addList(){
+    // this.router.navigateByUrl('/tabs/tab1/add');
+    const alert = await this.alertCtrl.create({
+      header: 'New List',
+      inputs: [
+        {
+          name: 'title',
+          type: 'text',
+          placeholder: 'List name'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        }, {
+          text: 'Ok',
+          handler: (data) => {
+            console.log(data);
+            if (data.title.length === 0) {
+              return;
+            } 
+            this.wishesService.createList(data.title);
+          }
+        }
+      ]
+    });
+
+    alert.present();
   }
 
 }
